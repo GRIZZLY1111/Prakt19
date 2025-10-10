17f6028e79ded884ddb8f418667fecd126543ba9
zemerovaanna <zemerovagmail.com>
2025-10-10 09:46:06 +0500

    first commit

--- Prakt19/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
+++ Prakt19/app/src/main/java/com/example/criminalintent/CrimeFragment.kt
@@ -1,5 +1,6 @@
 package com.example.criminalintent
 
+import android.icu.text.SimpleDateFormat
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
@@ -9,30 +10,34 @@ import android.view.ViewGroup
 import android.widget.Button
 import android.widget.CheckBox
 import android.widget.EditText
+import android.widget.TextView
 import androidx.fragment.app.Fragment
 import com.example.criminalintent.Crime.Crime
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.google.android.material.snackbar.Snackbar
 import java.util.Date
+import java.util.Locale
 
 class CrimeFragment: Fragment() {
     private lateinit var crime: Crime
     private lateinit var titleField: EditText
-    private lateinit var dateButton: Button
+    private lateinit var dateButton: FloatingActionButton
+    private lateinit var crimetext: TextView
     private lateinit var solvedCheckBox: CheckBox
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         crime = Crime(title = "", date = Date())
     }
-    override fun onCreateView( inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle? ): View?
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle? ): View?
     { val view = inflater.inflate(R.layout.fragment_crime, container, false)
-        titleField =
-            view.findViewById(R.id.crime_title) as EditText
-        dateButton = view.findViewById(R.id.crime_date) as Button
+        titleField = view.findViewById(R.id.crime_title) as EditText
+        dateButton = view.findViewById(R.id.crime_date) as FloatingActionButton
         solvedCheckBox = view.findViewById(R.id.crime_solved) as CheckBox
-            dateButton.apply {
-                    text = crime.date.toString()
-                isEnabled = false
-        }
+        crimetext = view.findViewById(R.id.crime_text) as TextView
+        crimetext.apply { text = crime.date.toString() }
         return view
     }
     override fun onStart() {
@@ -67,12 +72,16 @@ class CrimeFragment: Fragment() {
                 "Преступление отмечено как нерешённое"
             }
             if (isChecked){
-                dateButton.isEnabled=true
+                dateButton.isClickable=true
             }
             else{
-                dateButton.isEnabled=false
+                dateButton.isClickable=false
             }
             Snackbar.make(requireView(), message, Snackbar.LENGTH_SHORT).show()
         }
+        val date = Date()
+        val format = SimpleDateFormat("EEEE, dd MMMM yyyy", Locale("ru"))
+        val formateDate = format.format(date)
+        crimetext.text = formateDate
     }
 }
